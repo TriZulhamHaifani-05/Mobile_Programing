2d0b796add96492a34c1a3bd2b000cd8d4c9a42e
TriZulhamHaifani-05 <80773818+TriZulhamHaifani-05@users.noreply.github.com>
2021-10-13 16:05:49 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,65 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
-class Tampilan extends StatelessWidget {
-Widget build(BuildContext context) {
-return MaterialApp(
-home: Scaffold(
-appBar: AppBar(
-leading: const IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-title: Text('Aplikasi Sederhana'),
-),
-body: Center(child: Text('Welcome Back My Channel')),
-backgroundColor: Colors.blueGrey.shade200,
-));
+
+class LayoutRow extends StatelessWidget {
+  const LayoutRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              flexibleSpace: SafeArea(
+                  // ignore: avoid_unnecessary_containers
+                  child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        IconButton(
+                          icon: Icon(Icons.menu),
+                          tooltip: 'Navigation menu',
+                          onPressed: null, // null disables the button
+                        ),
+                        Spacer(),
+                        Text(
+                          'Kantin Politeknik Kampar',
+                          textAlign: TextAlign.center,
+                        ),
+                        Spacer()
+                      ],
+                    )
+                  ],
+                ),
+              )),
+            ),
+            // ignore: avoid_unnecessary_containers
+            body: Container(
+                child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      'Nasi Goreng',
+                      textAlign: TextAlign.left,
+                    ),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(
+                          'lib/assets/gambar/nasigoreng.jpg'
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ))
+            ));
+  }
 }
-}
+
 void main() {
-runApp(Tampilan());
+  runApp(LayoutRow());
 }
